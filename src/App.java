b8d4da2daf6acc07d400a8b7b642032d668fff1f
Philipp Kotte <minephil31@gmail.com>
2022-11-15 00:48:42 +0100

    Updatet App.java

--- src/App.java
+++ src/App.java
@@ -1,2 +1,12 @@
+
+import facade.Banksystem;
+import ui.Ui;
+
 public class App {
-}
+						public static void main(String[] args) throws Exception {
+									Banksystem bs = new Banksystem("Spaßkasse Mannheim");
+									new Ui(bs);
+						}
+
+			}
+
