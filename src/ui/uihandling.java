50261782d795035ee19fa78214e09bc7921e8a96
Philipp Kotte <minephil31@gmail.com>
2022-11-16 10:16:40 +0100

    updated uihandling

--- src/ui/uihandling.java
+++ src/ui/uihandling.java
@@ -1,5 +1,6 @@
 package ui;
 import domain.Kunde;
+import facade.Banksystem;
 
 import java.util.Scanner;
 
@@ -7,6 +8,9 @@ public class uihandling {
     
    
         private void login(){
+
+        mainloop:
+            while(true){
             Scanner sc = new Scanner(System.in);
             String[] options = {"Anmelden", "Registrieren", "beenden"};
             for(String s: options){
@@ -16,15 +20,26 @@ public class uihandling {
             int input = Integer.parseInt(sc.nextLine());
 
             switch(input){
-                case 1:
-                case 2:
-                case 3:
+                case 1: anmelden(sc); break;
+                case 2: registrieren(); break;
+                case 3: break mainloop;
                 default:
             }
         }
+        }
 
         //future return type is Kunde
-        private void anmelden(){
+        private void anmelden(Scanner sc){
+            String[] options = {"Bitte geben Sie ihre Kundennummer an: ", "Bitte geben Sie ihr Password an: "};
+            System.out.println(options[0]);
+            System.out.print(" > ");
+            int kn = Integer.parseInt(sc.nextLine());
+            System.out.println(options[1]);
+            System.out.print(" > ");
+            String pw = sc.nextLine();
+
+
+
 
         }
         //future return type is Kunde
