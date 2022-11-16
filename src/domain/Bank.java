00e0e681a38a2f2241e746ffd2fc78289c84bbc7
Philipp Kotte <minephil31@gmail.com>
2022-11-16 10:17:47 +0100

    aktualisierte Methoden für Kunden

--- src/domain/Bank.java
+++ src/domain/Bank.java
@@ -7,11 +7,16 @@ import java.util.HashMap;
 public class Bank implements Serializable {
 			private String name;
 			private HashMap<Integer, Konto> konten = new HashMap<>();
+			//neue Hashmap für die Verwaltung aller Kudnen
+			private HashMap<Integer, Kunde> kunden = new HashMap<>();
 			private int kontozähler;
+			//zähler zum anlegen von Kudnen
+			private int kundenzaehler;
 
 			public Bank(String name) {
 						this.name = name;
 						this.kontozähler = -1;
+						this.kundenzaehler = -1;
 			}
 
 			public int addKonto(String name, int auswahl) {
@@ -38,6 +43,21 @@ public class Bank implements Serializable {
 			public Konto findeKonto(int kontonummer) {
 						return konten.get(kontonummer);
 			}
+			//Kunden in der hashmap finden und zurückgeben
+			public Kunde finde_kunden(int kn){
+				return kunden.get(kn);
+			}
+			//Passwort des Kundens bekommen
+			public String get_Password(int kn){
+				Kunde k = kunden.get(kn);
+				return k.get_password();
+			}
+			//Kunde zum System hinzufügen
+			public int adde_kunden(String first_name, String last_name, String adress, String password, int age){
+				Kunde kunde = new Kunde(first_name, last_name, adress, password, age, ++kundenzaehler);
+				kunden.put(kunde.get_client_number(), kunde);
+				return kunde.get_client_number();
 
+			}
 }
 
